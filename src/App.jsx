fe41989ae509d429bc4302df319d007747a838cb
fenilprajapati24 <fenilprajapati9334@gmail.com>
2026-05-27 17:35:09 +0530

    fix import

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from './components/Header';
 import Footer from'./components/Footer';
 import Sidebar from './components/Sidebar';
-import CreatePost from './components/createPost';
+import CreatePost from './components/CreatePost';
 import PostList from './components/PostList';
 import { useState } from 'react';
 import PostListProvider from './store/post-list-store';
